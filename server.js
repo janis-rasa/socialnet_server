b5ea1b9c3aa577f616e56be37cfa6c3c8b573bf0
Janis Rasins <janis@developer.lv>
2022-10-06 09:30:22 +0300

    Adding Posts page v.1.2.0

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // server.js
+
+// Config
 const jsonServer = require("json-server")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
+const rewriteRules = jsonServer.rewriter({
+	"/messages/:userId": "/messages?userId=:userId",
+})
 const middlewares = jsonServer.defaults()
 
+server.use(rewriteRules)
 server.use(middlewares)
 server.use(router)
+
+// Port
 server.listen(5000, () => {
 	console.log("JSON Server is running")
 })
